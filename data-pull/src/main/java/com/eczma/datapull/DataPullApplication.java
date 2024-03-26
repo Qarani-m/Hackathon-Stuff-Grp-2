daada5e8c1d153effdd23e8d1a2cb8bbdbb67086
qarani-m <emqarani@gmail.com>
2024-03-26 12:19:47 +0300

    inittial

---
+++ data-pull/src/main/java/com/eczma/datapull/DataPullApplication.java
@@ -0,0 +1,13 @@
+package com.eczma.datapull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPullApplication.class, args);
+	}
+
+}
